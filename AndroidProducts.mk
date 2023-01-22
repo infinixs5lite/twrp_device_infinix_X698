10293a5525762b992061108aa85ad9afee9b3197
Abdullashakee <123253935+Abdullashakee@users.noreply.github.com>
2023-01-22 11:56:04 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X698.mk
+    $(LOCAL_DIR)/twrp_Infinix-X698.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X698-user \
-    omni_Infinix-X698-userdebug \
-    omni_Infinix-X698-eng
+    twrp_Infinix-X698-user \
+    twrp_Infinix-X698-userdebug \
+    twrp_Infinix-X698-eng
