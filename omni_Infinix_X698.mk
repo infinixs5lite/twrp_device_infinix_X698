c7b6cc5cfe2fb82ec7b4cb11cc679d6b289d54ee
Abdullashakee <123253935+Abdullashakee@users.noreply.github.com>
2023-01-22 11:23:27 +0530

    Update and rename omni_Infinix-X698.mk to omni_Infinix_X698.mk

--- omni_Infinix_X698.mk
+++ omni_Infinix_X698.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/infinix/Infinix-X698/device.mk)
 
 PRODUCT_DEVICE := Infinix-X698
-PRODUCT_NAME := omni_Infinix-X698
+PRODUCT_NAME := omni_Infinix_X698
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X698
 PRODUCT_MANUFACTURER := infinix
