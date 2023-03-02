36106e33605cb4f86ba27227898377f5a72f9a4b
Abdullashakee <123253935+Abdullashakee@users.noreply.github.com>
2023-03-02 16:33:12 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix_X698
+DEVICE_PATH := device/infinix/X698
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
