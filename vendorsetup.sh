ad72035884cf558b244b4430b90367984367f994
Abdullashakee <123253935+Abdullashakee@users.noreply.github.com>
2023-03-02 19:59:18 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_Infinix_X698-user
-add_lunch_combo twrp_Infinix_X698-userdebug
-add_lunch_combo twrp_Infinix_X698-eng
+add_lunch_combo twrp_X698-user
+add_lunch_combo twrp_X698-userdebug
+add_lunch_combo twrp_X698-eng
